402f6a4474525ef9011375656854c1e00c14aac0
Ash <ashermanning@yahoo.co.uk>
2018-03-06 23:27:00 +0200

    first release

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # i3blocks-multicore
-A multicore CPU monitor block for i3blocks.
+A multicore CPU monitor blocklet for i3blocks.
+
+## Installation
+1. Install F#
+2. Clone this repository
+3. run `fsharpc multicore.fsx` obtaining multicore.exe and a copy of FSharp.Core.dll.
+4. Add to your i3blocks config. You will need to enable pango markup, with the `markup=pango` property. If your shell automatically executes .exe files with mono, then you can place multicore.exe and FSharp.Core.dll in your i3blocks script folder, and the blocklet will work if the title is `[multicore.exe]`. Otherwise, use the `command=mono multicore.exe`, replacing the paths for both mono and multicore.exe as necessary.
---
+++ multicore.fsx
@@ -0,0 +1,49 @@
+open System.Diagnostics
+
+let  lowThreshold = 25 // cpu values below this are "low"
+let highThreshold = 65 // cpu values above this are "high"
+
+let  lowColor = "#00FF00" // the color for low cpu usage
+let  medColor = "#FFD500" // the color for medium cpu usage
+let highColor = "#FF0000" // The color for high cpu usage
+
+// return a pango string with a specified text color
+let pangoColor (color:string) (s:string) : string =
+    sprintf "<span foreground=\"%s\">%s</span>" color s
+
+// get the number of cores
+let ncores = System.Environment.ProcessorCount
+
+// gets a monitor for the nth logical core
+let getCoreMonitor (n:int) : PerformanceCounter =
+    new PerformanceCounter("Processor", "% Processor Time", n.ToString())
+
+let coreMonitors : PerformanceCounter [] =
+    Array.init ncores getCoreMonitor
+    // get the values, and discard them.
+    // The next time they are used, they will be correct.
+    |> Array.map (fun monitor -> ignore (monitor.NextValue()); monitor)
+
+// sleep for 1 second, so that the values are accurate.
+System.Threading.Thread.Sleep(1000);
+
+// print the usage for one core.
+// by default, the usage is truncated to the (0-9) range.
+let printUsage (monitor:PerformanceCounter) : unit =
+    // usage as an integer
+    let usage = monitor.NextValue()
+                |> floor
+                |> int32
+
+    if usage >= 100 then failwith "core usage can't be above 100%!" else
+    let color =
+        if usage > highThreshold then highColor
+        elif usage >= lowThreshold then medColor
+        else lowColor
+
+    usage.ToString()
+    |> pangoColor color
+    |> printf "%s"
+
+
+coreMonitors |> Array.iter printUsage
