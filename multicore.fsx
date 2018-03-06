7c28b85bf780906eaf8b5c4a12deb39d19098737
Ash <ashermanning@yahoo.co.uk>
2018-03-07 00:50:45 +0200

    add hex encoding mode

--- multicore.fsx
+++ multicore.fsx
@@ -7,6 +7,19 @@ let  lowColor = "#00FF00" // the color for low cpu usage
 let  medColor = "#FFD500" // the color for medium cpu usage
 let highColor = "#FF0000" // The color for high cpu usage
 
+type encoding =
+    | Dec // decimal encoding (0-9)
+    | Hex // hex encoding (0-F)
+
+let encoding = Dec
+// let encoding = Hex
+
+// encodes a cpu core usage in the (0-99) range as a string.
+let encode (usage:int) : string =
+    match encoding with
+    | Dec -> (usage / 10).ToString()
+    | Hex -> (usage / 16).ToString("X")
+
 // return a pango string with a specified text color
 let pangoColor (color:string) (s:string) : string =
     sprintf "<span foreground=\"%s\">%s</span>" color s
@@ -28,7 +41,6 @@ let coreMonitors : PerformanceCounter [] =
 System.Threading.Thread.Sleep(1000);
 
 // print the usage for one core.
-// by default, the usage is truncated to the (0-9) range.
 let printUsage (monitor:PerformanceCounter) : unit =
     // usage as an integer
     let usage = monitor.NextValue()
@@ -36,12 +48,13 @@ let printUsage (monitor:PerformanceCounter) : unit =
                 |> int32
 
     if usage >= 100 then failwith "core usage can't be above 100%!" else
+
     let color =
         if usage > highThreshold then highColor
         elif usage >= lowThreshold then medColor
         else lowColor
 
-    (usage / 10).ToString()
+    encode usage
     |> pangoColor color
     |> printf "%s"
 
